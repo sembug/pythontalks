abcad086b5d332254c6f71f3f82e6e074ca889fa
Roberto Nunes <roberto.nunes@gmail.com>
2016-07-19 22:05:51 -0300

    Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='PythonTalks',
+      version='1.0',
+      description='OpenShift App',
+      author='Roberto Nunes',
+      author_email='ranunes@msn.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Flask==0.10.1'],
+     )
